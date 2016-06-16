ba3b0c5bda9efa10c35219716cb0c19f04bbf017
Maria <mmorrison1978@gmail.com>
2016-06-16 09:35:23 -0600

    updated

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var classCycle=['imageCycle1','imageCycle2', 'imageCycle3', 'imageCycle4', 'imageCycle5', 'imageCycle6', 'imageCycle7', 'imageCycle8'];
+  var classCycle=['imageCycle1','imageCycle2', 'imageCycle3', 'imageCycle4', 'imageCycle5', 'imageCycle6', 'imageCycle7', 'imageCycle8', 'imageCycle9', 'imageCycle10'];
   var randomNumber = Math.floor(Math.random() * classCycle.length);
   var classToAdd = classCycle[randomNumber];
   $('body').addClass(classToAdd);
