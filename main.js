b5944e01d88f9ac991b7b0d04c577a9763e928ef
Maria <mmorrison1978@gmail.com>
2016-06-15 20:20:19 -0600

    added random background images

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var classCycle=['imageCycle1','imageCycle2', 'imageCycle3', 'imageCycle4', 'imageCycle5', 'imageCycle6'];
+    var randomNumber = Math.floor(Math.random() * classCycle.length);
+    var classToAdd = classCycle[randomNumber];
+    $('body').addClass(classToAdd);
+
+});
