5d3fb3ca6e9b3f0b858c8b765ea10205a78b0a4d
Maria <mmorrison1978@gmail.com>
2016-06-17 12:51:16 -0600

    updated

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ $(document).ready(function(){
   var classCycle=['imageCycle1','imageCycle2', 'imageCycle3', 'imageCycle4', 'imageCycle5', 'imageCycle6', 'imageCycle7', 'imageCycle8', 'imageCycle9', 'imageCycle10'];
   var randomNumber = Math.floor(Math.random() * classCycle.length);
   var classToAdd = classCycle[randomNumber];
+  var id = Math.floor(Math.random() * 7);
+  var rando = "https://quotes-generator.herokuapp.com/random/" + id;
+
   $('body').addClass(classToAdd);
 
   $('.new').on('click', function() {
-    var id = Math.floor(Math.random() * 7);
-    $.getJSON("https://quotes-generator.herokuapp.com/random/:" + id, function(result) {
-      console.log(result);
-      $('.content').html(JSON.stringify(result));
-
+    $.getJSON("https://quotes-generator.herokuapp.com/all", function(result) {
+      $(".content").html(result);
     })
   });
+
 });
