3e74c025a4b55b3b8af6492c8850796e608be21d
Maria <mmorrison1978@gmail.com>
2016-06-17 16:29:38 -0600

    added more quotes

--- main.js
+++ main.js
@@ -2,15 +2,31 @@ $(document).ready(function(){
   var classCycle=['imageCycle1','imageCycle2', 'imageCycle3', 'imageCycle4', 'imageCycle5', 'imageCycle6', 'imageCycle7', 'imageCycle8', 'imageCycle9', 'imageCycle10'];
   var randomNumber = Math.floor(Math.random() * classCycle.length);
   var classToAdd = classCycle[randomNumber];
-  var id = Math.floor(Math.random() * 7);
+  var id = Math.floor(Math.random() * 13);
   var rando = "https://quotes-generator.herokuapp.com/random/" + id;
 
   $('body').addClass(classToAdd);
 
   $('.new').on('click', function() {
-    $.getJSON("https://quotes-generator.herokuapp.com/all", function(result) {
-      $(".content").html(result);
+    $.getJSON(rando, function(result) {
+      console.log(result.quote);
+      $(".quote").html(result.quote);
+      $('.author').html(result.author);
+      $('.title').html(result.book)
     })
   });
+
+  function getQuote(){
+    $.ajax({
+      url: rando,
+      success: function(response) {
+        console.log(response);
+        var r = JSON.parse(response);
+        currentQuote = r.quote;
+        currentAuthor = r.author;
+        if(inIframe())
+      }
+    });
+  }
 
 });
