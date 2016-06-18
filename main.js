1eb22bdc851e39d52e5ca0bdc89b4d525d6e99db
Maria <mmorrison1978@gmail.com>
2016-06-18 07:20:17 -0600

    tweet link is working

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
   var classCycle=['imageCycle1','imageCycle2', 'imageCycle3', 'imageCycle4', 'imageCycle5', 'imageCycle6', 'imageCycle7', 'imageCycle8', 'imageCycle9', 'imageCycle10'];
   var randomNumber = Math.floor(Math.random() * classCycle.length);
   var classToAdd = classCycle[randomNumber];
-  // var id = Math.floor(Math.random() * 13);
 
+  function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+};
 
   $('body').addClass(classToAdd);
 
@@ -28,8 +30,13 @@ function getQuote(){
     $('.quote, .author').empty();
     $('.quote').append(quote);
     $('.author').append(author);
-    $('.fa-twitter').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + quote + '" ' + author));
-    $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(author)+'&content=' + encodeURIComponent(quote));
+
+    $('.tweet').on('click', function() {
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=mmm5225&text=' + encodeURIComponent('"' + quote + '" ' + author));
+    })
+
+    $('.tweet').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=mmm5225&text=' + encodeURIComponent('"' + quote + '" ' + author));
+    $('.tumblr').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,mmm5225&caption='+encodeURIComponent(author)+'&content=' + encodeURIComponent(quote));
 
   });
 }
