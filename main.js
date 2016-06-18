b37ca7ff9a1c33ba709f7b670d3cd63013266c0e
Maria <mmorrison1978@gmail.com>
2016-06-17 19:51:57 -0600

    updated

--- main.js
+++ main.js
@@ -2,17 +2,34 @@ $(document).ready(function(){
   var classCycle=['imageCycle1','imageCycle2', 'imageCycle3', 'imageCycle4', 'imageCycle5', 'imageCycle6', 'imageCycle7', 'imageCycle8', 'imageCycle9', 'imageCycle10'];
   var randomNumber = Math.floor(Math.random() * classCycle.length);
   var classToAdd = classCycle[randomNumber];
-  var id = Math.floor(Math.random() * 13);
-  var rando = "https://quotes-generator.herokuapp.com/random/" + id;
+  // var id = Math.floor(Math.random() * 13);
+
 
   $('body').addClass(classToAdd);
 
   $('.new').on('click', function() {
-    $.getJSON(rando, function(result) {
-      console.log(result.quote);
-      $(".quote").html(result.quote);
-      $('.author').html(result.author);
-      $('.title').html(result.book)
-    })
+    getQuote();
   });
 });
+
+function getQuote(){
+  var id = Math.floor(Math.random() * 13);
+  var retrieveData = $.ajax({
+    url: "https://quotes-generator.herokuapp.com/random/" + id,
+    type: 'GET',
+    dataType: 'json',
+  });
+
+  retrieveData.done(function(data) {
+    var quote = data.quote;
+    var author = data.author;
+    var title = data.book;
+
+    $('.quote, .author').empty();
+    $('.quote').append(quote);
+    $('.author').append(author);
+    $('.fa-twitter').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + quote + '" ' + author));
+    $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(author)+'&content=' + encodeURIComponent(quote));
+
+  });
+}
