b2ced98f61020050e38d560b29fc098ba7907c88
Maria <mmorrison1978@gmail.com>
2016-06-26 20:25:47 -0600

    added more quotes

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 });
 
 function getQuote(){
-  var id = Math.floor(Math.random() * 16);
+  var id = Math.floor(Math.random() * 21);
   var retrieveData = $.ajax({
     url: "https://quotes-generator.herokuapp.com/random/" + id,
     type: 'GET',
