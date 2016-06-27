ba8372f6c2af42047c17cf1a1b405babc35b6a39
Maria <mmorrison1978@gmail.com>
2016-06-26 20:08:56 -0600

    added new quote

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 });
 
 function getQuote(){
-  var id = Math.floor(Math.random() * 13);
+  var id = Math.floor(Math.random() * 14);
   var retrieveData = $.ajax({
     url: "https://quotes-generator.herokuapp.com/random/" + id,
     type: 'GET',
