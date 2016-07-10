0c12a1d54d9cfb8ad7c272532023127e6eed7173
Maria <mmorrison1978@gmail.com>
2016-07-10 16:41:18 -0600

    updated

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 });
 
 function getQuote(){
-  var id = Math.floor(Math.random() * 28);
+  var id = Math.floor(Math.random() * 29);
   var retrieveData = $.ajax({
     url: "https://quotes-generator.herokuapp.com/random/" + id,
     type: 'GET',
